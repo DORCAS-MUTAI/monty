eeec821f2d36b5b4fdb0139adf54d7e98f45187e
DORCAS-MUTAI <dorcasjmutai@gmail.com>
2023-08-24 04:18:48 -0700

    update

--- monty.h
+++ monty.h
@@ -5,6 +5,9 @@ void push_func(stack_t **head, unsigned int counter);
 void pall_func(stack_t **head, unsigned int counter);
 void pint_func(stack_t **head, unsigned int counter);
 void pop_func(stack_t **head, unsigned int counter);
+void swap_func(stack_t **head, unsigned int counter);
+void add_func(stack_t **head, unsigned int counter);
+void nop_func(stack_t **head, unsigned int counter);
 
 
 #endif
