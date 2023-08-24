963fa98a2b6e62ce028575a2e92d4a89712ac3ea
DORCAS-MUTAI <dorcasjmutai@gmail.com>
2023-08-24 04:09:45 -0700

    pop

--- monty.h
+++ monty.h
@@ -2,7 +2,9 @@
 #define MONTY_H
 
 void push_func(stack_t **head, unsigned int counter);
-void f_pall(stack_t **head, unsigned int counter);
+void pall_func(stack_t **head, unsigned int counter);
 void pint_func(stack_t **head, unsigned int counter);
+void pop_func(stack_t **head, unsigned int counter);
+
 
 #endif
